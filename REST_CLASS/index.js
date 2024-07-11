bd8f5e41a5f57c0cc71a413c3382901e9d85c2cb
mradul-bansal <9922102073@mail.jiit.ac.in>
2024-07-12 01:06:59 +0530

    Created a basic layout

--- REST_CLASS/index.js
+++ REST_CLASS/index.js
@@ -8,28 +8,28 @@ app.use(express.urlencoded({ extended: true}));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.set(express.static (path.join(__dirname, "public")));
+app.use(express.static (path.join(__dirname, "public")));
 
-let post = [
+let posts = [
     {
         username: "mradulbansal",
-        content: "Pari pagal h"
+        content: "Pari pagal h",
 
     },
     {
         username: "hiralbansal",
-        content: "hunny pagal h"
+        content: "hunny pagal h",
 
     },
     {
         username: "manishbansal",
-        content: "dono pagal h"
+        content: "dono pagal h",
 
     }
 ];
 
-app.get("/", (req, res) => {
-  res.send("Server Working Well!");
+app.get("/posts", (req, res) => {
+  res.render("index.ejs", {posts});
 })
 
 app.listen(port, () => {
